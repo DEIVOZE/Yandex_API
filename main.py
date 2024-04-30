0bceec0071e2e14e4a58f36b21c93c9bd1b6f0bc
DEIRIl <153659139+DEIRIl@users.noreply.github.com>
2024-04-30 21:09:31 +0300

    Часть №5

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from io import BytesIO
 
 import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
 from PyQt5.uic.properties import QtCore
 
 SCREEN_SIZE = [600, 600]
@@ -20,6 +21,7 @@ class Example(QWidget):
         self.maps = [('Схема', 'map'), ('Спутник', 'sat'), ('Гибрид', 'sat,skl')]
         self.l = 0
         self.initUI()
+        self.need_point = False
 
     def getImage(self):
         map_request = f"http://static-maps.yandex.ru/1.x/"
@@ -27,8 +29,11 @@ class Example(QWidget):
         map_params = {
             "ll": f"{self.ll1},{self.ll2}",
             "spn": f"{self.spn},{self.spn}",
-            "l": self.maps[self.l][1]
+            "l": self.maps[self.l][1],
         }
+
+        if self.need_point:
+            map_params['pt'] = f"{self.need_point},pm2rdm"
 
         response = requests.get(map_request, params=map_params)
 
@@ -39,9 +44,7 @@ class Example(QWidget):
             sys.exit(1)
 
         # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
+        self.map_file = BytesIO(response.content).getvalue()
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
@@ -57,55 +60,103 @@ class Example(QWidget):
         self.choose_map_layer.resize(100, 100)
         self.choose_map_layer.clicked.connect(self.choose_layer)
 
+        self.label = QLabel('Введите запрос для поиска', self)
+        self.label.move(130, 460)
+        self.label.resize(300, 30)
+
+        self.search = QLineEdit(self)
+        self.search.move(130, 490)
+        self.search.resize(300, 30)
+
+        self.search_btn = QPushButton('Искать', self)
+        self.search_btn.move(130, 525)
+        self.search_btn.resize(300, 35)
+        self.search_btn.clicked.connect(self.get_coords)
+
     def paintEvent(self, event):
-        if self.rneed:
-            self.getImage()
-            self.pixmap = QPixmap(self.map_file)
-            self.image.setPixmap(self.pixmap)
-            self.rneed = False
+        try:
+            if self.rneed:
+                self.getImage()
+                self.pixmap = QPixmap()
+                self.pixmap.loadFromData(self.map_file)
+                self.image.setPixmap(self.pixmap)
+                self.label.setText('Введите запрос для поиска')
+                self.rneed = False
+        except Exception as e:
+            print(e)
 
     def choose_layer(self):
         self.l = (self.l + 1) % 3
         self.choose_map_layer.setText(self.maps[self.l][0])
         self.rneed = True
 
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
 
     def keyPressEvent(self, event):
-        # У меня на ноутбуке не было клавиш PageUp и PageDown, так что я вот так выкручивался
-        if event.key() in (Qt.Key_Z, Qt.Key_PageUp):
-            if self.spn < 5:
-                self.spn *= 2
+        try:
+            # У меня на ноутбуке не было клавиш PageUp и PageDown, так что я вот так выкручивался
+            if event.key() in (Qt.Key_Z, Qt.Key_PageUp, 1071):
+                if self.spn < 5:
+                    self.spn *= 2
+                    self.rneed = True
+            elif event.key() in (Qt.Key_X, Qt.Key_PageDown, 1063):
+                if self.spn > 0.001:
+                    self.spn /= 2
+                    self.rneed = True
+            # В PyQt если есть кнопка на форме, то стрелки почему-то не вызывают функцию,
+            # так что я вместо стрелок использовал WASD
+            elif event.key() in (Qt.Key_Up, 1062, Qt.Key_W):
+                if self.ll2 + self.spn / 2 < 90 - self.spn // 2:
+                    self.ll2 += self.spn / 2
+                    self.rneed = True
+            elif event.key() in (Qt.Key_Down, Qt.Key_S, 1067):
+                if self.ll2 - self.spn / 2 > -90 + self.spn // 2:
+                    self.ll2 -= self.spn / 2
+                    self.rneed = True
+            elif event.key() in (Qt.Key_Right, Qt.Key_D, 1042):
+                if self.ll1 + self.spn / 2 < 180:
+                    self.ll1 += self.spn / 2
+                else:
+                    self.ll1 = 179.99999
                 self.rneed = True
-        elif event.key() in (Qt.Key_X, Qt.Key_PageUp):
-            if self.spn > 0.001:
-                self.spn /= 2
+            elif event.key() in (Qt.Key_Left, Qt.Key_A, 1060):
+                if self.ll1 - self.spn / 2 > -180:
+                    self.ll1 -= self.spn / 2
+                else:
+                    self.ll1 = -180
                 self.rneed = True
-        # В PyQt если есть кнопка на форме, то стрелки почему-то не вызывают функцию,
-        # так что я вместо стрелок использовал WASD
-        elif event.key() in (Qt.Key_Up, Qt.Key_W):
-            if self.ll2 + self.spn / 2 < 90 - self.spn // 2:
-                self.ll2 += self.spn / 2
-                self.rneed = True
-        elif event.key() in (Qt.Key_Down, Qt.Key_S):
-            if self.ll2 - self.spn / 2 > -90 + self.spn // 2:
-                self.ll2 -= self.spn / 2
-                self.rneed = True
-        elif event.key() in (Qt.Key_Right, Qt.Key_D):
-            if self.ll1 + self.spn / 2 < 180:
-                self.ll1 += self.spn / 2
+            self.repaint()
+        except Exception as e:
+            print(e)
+
+
+    def get_coords(self):
+        geocoder_request = (f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b"
+                            f"&geocode={self.search.text()}&format=json")
+
+        # Выполняем запрос.
+        try:
+            response = requests.get(geocoder_request)
+            if response:
+                json_response = response.json()
+                if json_response["response"]["GeoObjectCollection"]["featureMember"]:
+                    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                    # Полный адрес топонима:
+                    toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+                    # Координаты центра топонима:
+                    coords = toponym["Point"]["pos"]
+                    # print(f"{toponym_address} имеет координаты: {coords}")
+                    self.ll1, self.ll2 = map(float, coords.split())
+                    self.need_point = ','.join(coords.split())
+                    self.rneed = True
+                    self.repaint()
+                else:
+                    self.label.setText('Объект не найден')
             else:
-                self.ll1 = 179.99999
-            self.rneed = True
-        elif event.key() in (Qt.Key_Left, Qt.Key_A):
-            if self.ll1 - self.spn / 2 > -180:
-                self.ll1 -= self.spn / 2
-            else:
-                self.ll1 = -180
-            self.rneed = True
-        self.repaint()
+                print("Ошибка выполнения запроса:")
+                print(geocoder_request)
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
